bd077558d41a1c94b50d35fd65053c2a79188c8e
Zahadum <haitrung01@gmail.com>
2018-05-12 15:05:41 -0700

    test

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -21,8 +21,8 @@ class EmailController extends Controller
         $to      = 'haitrung01@gmail.com';
         $subject = 'Subscription Update: ';
         $message = 'hhh';
-        $headers = 'From: haitrung01@gmail.com' . "\r\n" .
-            'Reply-To: From: haitrung01@gmail.com' . "\r\n" .
+        $headers = 'From: contact@gwennguyen.com' . "\r\n" .
+            'Reply-To: From: contact@gwennguyen.com' . "\r\n" .
             "MIME-Version: 1.0\r\n" .
             "Content-Type: text/html; charset=ISO-8859-1\r\n";
         'X-Mailer: PHP/' . phpversion();
