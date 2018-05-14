c5441cc579d3d34b14f70b6012ed0c31eaaf85f8
Zahadum <haitrung01@gmail.com>
2018-05-14 16:13:37 -0700

    email bugs 4

--- app/Http/Controllers/Api/ReservationController.php
+++ app/Http/Controllers/Api/ReservationController.php
@@ -42,10 +42,10 @@ class ReservationController extends Controller
         $reservation = $this->validate($request,[
             'name' => 'required|min:3|max:50',
             'email'=> 'required|email',
-            /*'phone' => 'nullable',
+            'phone' => 'nullable',
             'people' => 'nullable',
-            'datetime' => 'nullable',
-            'note' => 'nullable',*/
+            'datetime' => 'nullable|date',
+            'note' => 'nullable',
         ]);
 
         $reservation = Reservation::create($reservation);
