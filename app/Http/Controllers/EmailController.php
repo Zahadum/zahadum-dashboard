8597fc1d58b3c0fe9525ba41ecc0d1245bdca32a
Zahadum <haitrung01@gmail.com>
2018-05-14 16:00:58 -0700

    test mail

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -20,12 +20,12 @@ class EmailController extends Controller
 */
         $to      = $mailData['email'];
         $subject = 'Subscription Update: '.$mailData['name'];
-        $message = 'Name: '.$mailData['name'];
-        $message .= 'Email: '.$mailData['email'];
-        $message .= 'Phone: '.$mailData['phone'];
-        $message .= 'People: '.$mailData['people'];
-        $message .= 'Reservation: '.$mailData['datetime'];
-        $message .= 'Created Date: '.$mailData['date'];
+        $message = 'Name: '.$mailData['name']. "\r\n";
+        //$message .= 'Email: '.$mailData['email']. "\r\n";
+        //$message .= 'Phone: '.$mailData['phone']. "\r\n";
+        //$message .= 'People: '.$mailData['people']. "\r\n";
+        //$message .= 'Reservation: '.$mailData['datetime']. "\r\n";
+        //$message .= 'Created Date: '.$mailData['date']. "\r\n";
         $headers = 'From: contact@gwennguyen.com' . "\r\n" .
             'Reply-To: From: contact@gwennguyen.com' . "\r\n" .
             "MIME-Version: 1.0\r\n" .
