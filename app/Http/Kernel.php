2835354e8a6b7c55907bc5f8de9caae9e3f1cbf3
Zahadum <haitrung01@gmail.com>
2018-05-18 11:48:43 -0700

    cors manually

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -59,6 +59,7 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'cors' => \Barryvdh\Cors\HandleCors::class,
+        //'cors' => \Barryvdh\Cors\HandleCors::class,
+        'cors' => \App\Http\Middleware\Cors::class,
     ];
 }
