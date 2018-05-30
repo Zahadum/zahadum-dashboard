f54d24d10f035f0aa73dbc67f19e702b1d19a325
Zahadum <haitrung01@gmail.com>
2018-05-30 14:04:06 -0700

    test server

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -62,8 +62,8 @@ class EmailController extends Controller
         $message = 'Name: '.$mailData['name']. "<br/>";
         $message .= 'Email: '.$mailData['email']. "<br/>";
         $message .= 'Note: '.$mailData['note']. "<br/>";
-        $headers = 'From: contact@vinacastudy.com' . "\r\n" .
-            'Reply-To: From: contact@vinacastudy.com' . "\r\n" .
+        $headers = 'From: contact@zahadum.tk' . "\r\n" .
+            'Reply-To: From: contact@zahadum.tk' . "\r\n" .
             "MIME-Version: 1.0\r\n" .
             "Content-Type: text/html; charset=ISO-8859-1\r\n";
         'X-Mailer: PHP/' . phpversion();
