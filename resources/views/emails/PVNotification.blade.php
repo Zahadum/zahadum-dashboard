30c7862940922bfc479a3718152461d2d9061dc7
Zahadum <haitrung01@gmail.com>
2018-06-01 16:22:44 -0700

    update mailer

--- resources/views/emails/PVNotification.blade.php
+++ resources/views/emails/PVNotification.blade.php
@@ -1,7 +1,7 @@
-Name: {{$name}}
-Email: {{$email}}
-Phone: {{$phone}}
-People: {{$people}}
-Reservation time: {{$datetime}}
-Note: {{$note}}
-Created Time: {{$date}}
+<div>Name: {{$name}}</div>
+<div>Email: {{$email}}</div>
+<div>Phone: {{$phone}}</div>
+<div>People: {{$people}}</div>
+<div>Reservation time: {{$datetime}}</div>
+<div>Note: {{$note}}</div>
+<div>Created Time: {{$date}}</div>
