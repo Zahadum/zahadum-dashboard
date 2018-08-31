c72bb2f945e59d83390bd6a6f6f2ab1b7106c570
Zahadum <haitrung01@gmail.com>
2018-08-31 08:51:36 -0700

    change queue method

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -9,8 +9,13 @@ class TestController extends Controller
     //
     public function TestMethod() {
         var_dump('aaa');
-        $mailData=[];
-        $mailTemplate=[];
+        $mailData['name'] = 'Trung';
+        $mailData['email'] = 'trunghangca@gmail.com';
+        $mailData['phone'] = '7789226612';
+        $mailData['subject'] = 'test';
+        $mailData['note'] = 'test note';
+
+        $mailTemplate='';
         $this->dispatch((new \App\Jobs\SendNotificationEmail($mailData,$mailTemplate)));
     }
 }
